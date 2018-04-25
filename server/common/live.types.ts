99430e80e09c456382fcfca7886feec451f69445
Rolf Bansbach <rolfisub@gmail.com>
2018-04-25 00:16:09 -0400

    softec live model interfaces

---
+++ server/common/live.types.ts
@@ -0,0 +1,66 @@
+export interface LiveEvent {
+    event_id: string;
+    event_name: string;
+    away_rot: string;
+    away_name: string;
+    home_rot: string;
+    home_name: string;
+    sport_id: string;
+    sport_name: string;
+    league_id: string;
+    league_name: string;
+    event_startdate: string;
+    extra_info: string;
+    streaming: string;
+    tv_channels: string;
+    is_sport_frozen: number;
+    is_league_frozen: number;
+    is_event_frozen: number;
+}
+
+export interface LiveMeta {
+    event_id: string;
+    meta_key: string;
+    meta_value: string;
+}
+
+export interface LiveOddsInfo {
+    event_id: string;
+    odd_id: string;
+    odd_name: string;
+    odd_text: string;
+    odd_type_id: string;
+    odd_type: string;
+    odd_subtype: string;
+    active: string;
+    handicap: string;
+    handicap_rest: string;
+    changed: string;
+    combinations: string;
+    is_balanced: string;
+    manual: number;
+    is_line_frozen: number;
+}
+
+export interface LiveOddsSelection {
+    event_id: string;
+    odd_id: string;
+    sel_id: string;
+    sel_name: string;
+    sel_odd: string;
+    active: string;
+    outcome: string;
+    void_factor: string;
+    player_id: number;
+    type_id: number;
+    type_name: string;
+    auto: number;
+    is_selection_frozen: string;
+}
+
+export interface LiveMsgModel {
+    events: LiveEvent[];
+    metas: LiveMeta[];
+    oddsinfo: LiveOddsInfo[];
+    oddsselections: LiveOddsSelection[];
+}
